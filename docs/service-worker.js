bc030c7ef9997387355a17a2cbccd793ebdbe54c
Sanjay <sanjaychinnavan42@gmail.com>
2025-11-30 08:56:02 +0530

    Add PWA support for Murugammal Tiffin Center

---
+++ docs/service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "mtc-pwa-v1";
+
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./images/mtc-logo-512.png"
+];
+
+// Install: cache basic assets
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+// Activate: clean old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Fetch: network first, fallback to cache
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
